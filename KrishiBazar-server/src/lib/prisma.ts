5a4ae8650f1e180895b82186efad8a7fe77a6bbf
pollob debnath <pullobdebnath@gmail.com>
2026-08-14 13:46:14 +0600

    fix prisma configuration

--- KrishiBazar-server/src/lib/prisma.ts
+++ KrishiBazar-server/src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
 import dotenv from "dotenv";
+import { PrismaClient } from "../generated/prisma/client";
 dotenv.config();
 
 const DATABASE_URL = process.env.DATABASE_URL;
